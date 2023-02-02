4be2f65540d8ee1c2fce1d560b73faadd4b0e08c
ulik312 <Ulik312@gmail.com>
2023-02-02 20:48:21 +0600

    update list

--- app/src/main/java/com/example/pixa/ImageAdapter.kt
+++ app/src/main/java/com/example/pixa/ImageAdapter.kt
@@ -26,6 +26,7 @@ class ImageAdapter(private val list: List<ImageModel>) : RecyclerView.Adapter<Im
     inner class ImageHolder(private val binding: ItemImageBinding) : RecyclerView.ViewHolder(binding.root){
         fun bind(imageModel: ImageModel) {
             binding.imageView.load(imageModel.largeImageURL)
+
         }
 
     }
